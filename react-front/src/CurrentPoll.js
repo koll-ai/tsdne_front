d1c3e61807fc5fcf32ddec0ea70ceed54106ee43
Phil <filouface@gmail.com>
2021-08-24 00:01:25 +0200

    fix not editzable component

--- react-front/src/CurrentPoll.js
+++ react-front/src/CurrentPoll.js
@@ -1,16 +1,15 @@
 import {useState} from "react";
 
 function UserForm(props){
-    const [value, setValue] = useState(props.value);
-    console.log("value : " + value);
+    // Champ de texte qui commencera toujours par la valeur de son prop starting_value
+    const [value, setValue] = useState(props.starting_value);
     return  <input type="text" name="user_prompt" value={value}
 
         onChange={event => {
-            if (event.target.value.startsWith(props.value)){
+            if (event.target.value.startsWith(props.starting_value)){
                 setValue(event.target.value);
             }
         }}
-        // onChange={event => console.log(event.target.value)}
     />;
 }
 
@@ -22,8 +21,7 @@ function CurrentPoll() {
         <p>Current Winning prompt :</p>
         <p> SCP 127-GPT is a burger that can turn people vegan</p>
         <p> Got an idea ? Go to http://www.strawpoll.fr to propose it !</p>
-        <UserForm value="SCP 102-GPT is a "
-                  bloc = "dala"
+        <UserForm starting_value="SCP 102-GPT is a "
                   />
     </div>
   );
