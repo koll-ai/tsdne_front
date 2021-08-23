c93e17ce5365901193ac0f79f0311cfea1e5361b
Phil <filouface@gmail.com>
2021-08-23 23:22:30 +0200

    added text component

--- react-front/src/CurrentPoll.js
+++ react-front/src/CurrentPoll.js
@@ -1,3 +1,12 @@
+import {useState} from "react";
+
+function UserForm(){
+    const {value, setValue} = useState(null);
+
+    return  <input type="text" name="user_prompt" value={value} />;
+}
+
+
 function CurrentPoll() {
   return (
     <div className="CurrentPoll">
@@ -5,7 +14,7 @@ function CurrentPoll() {
         <p>Current Winning prompt :</p>
         <p> SCP 127-GPT is a burger that can turn people vegan</p>
         <p> Got an idea ? Go to http://www.strawpoll.fr to propose it !</p>
-
+        <UserForm />
     </div>
   );
 }
