7683187e729e3a7829662d20919055979e523902
Phil <filouface@gmail.com>
2021-08-26 18:49:16 +0200

    fix api key

--- polling_api.py
+++ polling_api.py
@@ -4,7 +4,8 @@ import time
 from flask_cors import CORS
 
 MAX_PROMPT_LEN = 300
-NEXT_ROUND_KEY = open("/home/thisscpdoesnotexist/tsde/polling_api.key", "r").read()
+
+NEXT_ROUND_KEY = open("/home/thisscpdoesnotexist/tsde/polling_api.key", "r").read().rstrip()
 
 object_classes = ['Safe', 'Euclid', 'Keter', 'Thaumiel']
 
