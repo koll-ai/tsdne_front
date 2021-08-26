9460c476afaf31ca4208be03e811fda74e620639
Phil <filouface@gmail.com>
2021-08-26 14:55:41 +0200

    fix current scp number and next time

--- polling_api.py
+++ polling_api.py
@@ -121,8 +121,8 @@ def past_scps():
 
 @app.route('/current_scp_number/', methods=['GET'])
 def current_scp_number():
-    return scp_number
+    return "" + scp_number
 
 @app.route('/next_time/', methods=['GET'])
 def next_time():
-    return next_time
+    return "" + next_time
