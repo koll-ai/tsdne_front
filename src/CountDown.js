27591331104b98542c571b7a1633fff2f6c37331
Phil <filouface@gmail.com>
2021-09-03 00:51:23 +0200

    FIX time and can now count down

---
+++ src/CountDown.js
@@ -0,0 +1,51 @@
+import React from "react";
+
+const CountDown = ({ epoch }) => {
+
+  const duree_in_sec = Math.trunc((epoch - Date.now() )/ 1000);
+  console.log(duree_in_sec);
+
+  const hours = 0;
+  const minutes = Math.trunc( duree_in_sec / 60 );
+  const seconds = Math.trunc( duree_in_sec % 60  );
+
+  console.log((hours, minutes, seconds));
+
+  const [paused, setPaused] = React.useState(false);
+  const [over, setOver] = React.useState(false);
+  const [[h, m, s], setTime] = React.useState([hours, minutes, seconds]);
+
+  const tick = () => {
+    if (paused || over) return;
+    if (h === 0 && m === 0 && s === 0) setOver(true);
+    else if (m === 0 && s === 0) {
+      setTime([h - 1, 59, 59]);
+    } else if (s == 0) {
+      setTime([h, m - 1, 59]);
+    } else {
+      setTime([h, m, s - 1]);
+    }
+  };
+
+  const reset = () => {
+    setTime([parseInt(hours), parseInt(minutes), parseInt(seconds)]);
+    setPaused(false);
+    setOver(false);
+  };
+
+  React.useEffect(() => {
+    const timerID = setInterval(() => tick(), 1000);
+    return () => clearInterval(timerID);
+  });
+
+  return (
+    <div>
+      <p>{`${h.toString().padStart(2, '0')}:${m
+        .toString()
+        .padStart(2, '0')}:${s.toString().padStart(2, '0')}`}</p>
+
+    </div>
+  );
+}
+
+export default CountDown;
