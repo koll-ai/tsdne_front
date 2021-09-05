d25b900dd727d8bc71e367b495c502235d410095
Phil <filouface@gmail.com>
2021-09-05 18:04:05 +0200

    add author page

--- src/About.js
+++ src/About.js
@@ -3,8 +3,26 @@ import Footer from "./Footer";
 
 function About() {
   return (
-    <div className="App">
-      Made using the power of GPT-3
+    <div className="App" style={{textAlign: 'left', backgroundColor:'white' }}>
+      <h2>Authors</h2>
+
+        <ul>
+            <li>Ruben Grès</li>
+            <ul>
+                <li> Looking for a datascientist job in Germany</li>
+                <li>
+                    <a href={"https://www.linkedin.com/in/ruben-gres-484930158/"}>LinkedIn</a>
+                </li>
+            </ul>
+            <br/>
+            <li>Philippe Saadé</li>
+            <ul>
+                <li> Looking for a phd in Western Europe</li>
+                <li>
+                    <a href={"https://www.linkedin.com/in/philippe-saad%C3%A9-26972b149/"}>LinkedIn</a>
+                </li>
+            </ul>
+        </ul>
     </div>
   );
 }
