8d7d833dbd76cf6aa89ecd1093c0a3e2b1285607
Retronyme <rubengres@hotmail.fr>
2021-11-10 22:45:51 +0100

    execute as https

--- polling_api.py
+++ polling_api.py
@@ -237,44 +237,6 @@ def next_time_():
     """Renvoie le next time au format de javascript"""
     return str( math.trunc(next_time * 1000 ) )[0:-2] + "00"
 
-@app.route('/upvote/')
-def upvote():
-    ip = request.args.get('ip')
-    id_scp = request.args.get('id')
-
-    with open('upvotes.json') as json_file:
-        data = json.load(json_file)
-
-    if id_scp.isdigit():
-        id_scp = int(id_scp)
-        if id_scp < 9000 or id_scp > scp_number:
-            return Response(response="not a valid id", status=412)   
-    else:
-        return Response(response="not a valid id", status=412)   
-
-    if id_scp in data:
-        if ip not in data[id_scp]["ips"]:
-            data[id_scp]["n_upvotes"] += 1
-            data[id_scp]["ips"].append(ip)
-        else:
-            return Response(response="already voted", status=412)   
-    else:
-        data[id_scp] = {
-            "n_upvotes" : 1,
-            "ips" : [ip]
-        }
-        
-    with open('upvotes.json', 'w') as outfile:
-        json.dump(data, outfile)
-
-    return Response(response="upvote counted",status=200) 
-
-@app.route('/get_upvotes/')
-def get_upvotes():
-    with open('upvotes.json') as json_file:
-        data = json.load(json_file)
-    return data
-
 @app.route('/save_data/', methods=['GET'])
 def save_data():
     k = request.args.get('key')
