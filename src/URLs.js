bfc056181b5c90d106513cbcaa000777c1a3e58f
Phil <filouface@gmail.com>
2021-11-16 23:56:10 +0100

    debut login

--- src/URLs.js
+++ src/URLs.js
@@ -1,3 +1,4 @@
 //export const URL_API = "https://thisscpdoesnotexist.pythonanywhere.com/";
 export const URL_DB = "https://raw.githubusercontent.com/thisscpdoesnotexist/SCP-GPT_db/master/";
-export const URL_API = "http://51.75.255.134:45900/";
+// export const URL_API = "http://51.75.255.134:45900/";
+export const URL_API = "http://127.0.0.1:5000/";
