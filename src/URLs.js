b099e5c8f08ac9020702a68788c144f5b29fceb7
Phil <filouface@gmail.com>
2021-11-24 11:12:39 +0100

    add ssl

--- src/URLs.js
+++ src/URLs.js
@@ -1,4 +1,4 @@
-export const URL_DB = "http://51.75.255.134:45900/get_past_scp/?file=";
+export const URL_DB = "https://51.75.255.134:45900/get_past_scp/?file=";
 // export const URL_DB = "http://127.0.0.1:5000/get_past_scp/?file=";
-export const URL_API = "http://51.75.255.134:45900/";
+export const URL_API = "https://51.75.255.134:45900/";
 // export const URL_API = "http://127.0.0.1:5000/";
