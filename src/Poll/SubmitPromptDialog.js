bbd39c115f3c56cd5961290696936ea8b7036792
Phil <filouface@gmail.com>
2022-01-22 15:38:19 +0100

    added placeholder

--- src/Poll/SubmitPromptDialog.js
+++ src/Poll/SubmitPromptDialog.js
@@ -18,6 +18,8 @@ function UserForm(props){
     // Champ de texte qui commencera toujours par la valeur de son prop starting_value
     const [value, setValue] = useState(props.starting_value);
     return  <textarea type="text" name="user_prompt" value={value} fullWidth maxlength={300}
+                      
+                      placeholder={"an unlit platform staircase"}
 
         onChange={event => {
             if (event.target.value.startsWith(props.starting_value)){
