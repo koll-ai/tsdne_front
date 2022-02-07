865a8754db0bbc88dbc7c4bee3c70e9ce7ca29ca
Retronyme <rubengres@hotmail.fr>
2022-02-07 22:51:27 +0100

    error message

--- src/Poll/CurrentPoll.js
+++ src/Poll/CurrentPoll.js
@@ -137,6 +137,9 @@ function CurrentPoll() {
 
                     <br /><br />
                     If you're still  lost please check out our <a href="./about">FAQ</a>.
+                
+                    <br/>
+                    <b> We are currently having problems with our voting system, we are working on it right now ! </b>
                 </p>
             </div>
 
