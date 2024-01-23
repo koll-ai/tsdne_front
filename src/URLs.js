9e07f82e2983f11810628790aaebd04ba952f06c
PhilSad <philippe.henri.saade@gmail.com>
2024-01-23 22:04:51 +0000

    update

--- src/URLs.js
+++ src/URLs.js
@@ -1,5 +1,6 @@
-export const URL_DB = "http://51.75.255.134:45900/get_past_scp/?file=";
-export const URL_API = "http://51.75.255.134:45900/";
+export const URL_DB = "https://scpapi.koll.ai/get_past_scp/?file=";
+//export const URL_API = "http://scpapi.ml/";
+export const URL_API = "https://scpapi.koll.ai/";
 
 // export const URL_DB = "http://127.0.0.1:45900/get_past_scp/?file=";
 // export const URL_API = "http://127.0.0.1:45900/";
